70c99363eb157c2fdee7eebfd3577df0ee6f471c
Gustavo <gustavcampos73@gmail.com>
2021-09-27 20:48:32 -0300

    sorting firebase

---
+++ src/firebase/config.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDdNT7XUa4ql4zn2OoIszurbuRAATjpynY',
+  authDomain: 'vue-firebase-516e9.firebaseapp.com',
+  projectId: 'vue-firebase-516e9',
+  storageBucket: 'vue-firebase-516e9.appspot.com',
+  messagingSenderId: '677927298834',
+  appId: '1:677927298834:web:4181ff113724cd2238b5cb',
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const timestamp = firebase.firestore.FieldValue.serverTimestamp;
+const db = firebaseApp.firestore();
+export { db, timestamp };
---
+++ src/views/Welcome.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="welcome container">
+    <h1 class="title">Welcome</h1>
+    <div v-if="showLogin">
+      <h2 class="title">Login</h2>
+      <LoginForm />
+      <p>No acount yet? <span @click="showLogin = false">Signup</span> instead</p>
+    </div>
+    <div v-else>
+      <h2 class="title">SignUp</h2>
+      <signup-form />
+      <p>Already registered? <span @click="showLogin = true">Login</span> instead</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import SignupForm from '../components/SignupForm.vue';
+import LoginForm from '../components/LoginForm.vue';
+import { ref } from 'vue';
+export default {
+  components: { SignupForm, LoginForm },
+  setup() {
+    const showLogin = ref(true);
+
+    return { showLogin };
+  },
+};
+</script>
+
+<style>
+.welcome {
+  text-align: center;
+  padding: 20px 0;
+  font-family: 'Times New Roman', Times, serif;
+}
+/* form styles */
+.welcome form {
+  width: 300px;
+  margin: 20px auto;
+}
+.welcome label {
+  display: block;
+  margin: 20px 0 10px;
+}
+.welcome input {
+  width: 100%;
+  padding: 10px;
+  background-color: #484848;
+  border: 4px ridge #ccc;
+  outline: none;
+  color: white;
+  margin: 10px auto;
+  font-family: Tahoma, Geneva, Verdana, sans-serif;
+}
+
+.welcome span {
+  font-weight: bold;
+  text-decoration: underline;
+  cursor: pointer;
+}
+
+.welcome p {
+  color: #c0c0c0;
+}
+
+.welcome button {
+  margin: 20px auto;
+}
+</style>
